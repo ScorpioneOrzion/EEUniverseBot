86afa531934e7572ecc2707287f3e932bac83948
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-10-20 21:02:48 +0200

    final changes

--- EEUbot.js
+++ EEUbot.js
@@ -203,12 +203,9 @@ window.addEventListener("mouseup", e => {
 
 window.addEventListener("message", event => {
   if (event.origin !== "https://ee-universe.com/game/index.html" && event.origin !== "https://ee-universe.com") return
-  console.log(event)
   if (typeof event.data == "string") {
-    miniMapCanvas.width += 10
     const value = JSON.parse(event.data)
     if (value[1]) {
-      if (value[0] == 66) bottom.classList.toggle("min")
       ck("set", value[0], true)
     } else {
       ck("delete", value[0])
