2cacf71a830cd66616b89ee0d7687bafcfcf36e9
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-10-20 18:46:00 +0200

    version change

--- EEU_editor.user.js
+++ EEU_editor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EEU editor
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Editor in EEU
 // @author       ScorpioneOrzion
 // @match        https://ee-universe.com/*
