dbb62a75fdfb19532f3a3f585ff9c5c7b8b947a5
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-10-22 16:13:57 +0200

    token check

--- EEUbot.js
+++ EEUbot.js
@@ -193,6 +193,7 @@ window.addEventListener("message", event => {
   console.log(event.data)
   if (origin !== "https://ee-universe.com/game/index.html" && origin !== "https://ee-universe.com") return
   if (typeof event.data == "string") {
+    if (event.data.includes("token")) console.log("token received")
     const value = JSON.parse(event.data)
     if (value[1]) {
       ck("set", value[0], true)
