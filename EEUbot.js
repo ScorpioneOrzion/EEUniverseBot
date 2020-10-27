b3baf46d30d34280e55443f43ce145df9b303298
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-10-27 19:22:05 +0100

    removed movementLag

--- EEUbot.js
+++ EEUbot.js
@@ -4,58 +4,83 @@ import { Block } from './blocks.js'
 
 import { exp as EEUniverse } from './EEUniverse.js'
 
+//ui
 const top = document.querySelector('#topUi')
 const roomId = document.getElementById("roomId");
 const roomConnect = document.getElementById("roomIdConnect")
 const writeConnect = document.getElementById("writeData")
 const changeRoom = document.getElementById("changeroom")
+const areas = document.querySelectorAll('.ui.bottom>div:nth-child(1)>button')
+
+//images
+const image = new Image()
+image.src = './AllBlocks.png'
+const ground = new Image()
+ground.src = './ground.png'
+
+//canvas
 const gameCanvas = document.querySelector("body > canvas:nth-child(1)")
 const gameCtx = gameCanvas.getContext("2d");
 const miniMapCanvas = document.querySelector("body > canvas:nth-child(2)")
 const miniMapCtx = miniMapCanvas.getContext("2d");
 const ui = document.getElementById('ui');
 const uiCtx = ui.getContext("2d")
-const constants = {
-  x: 0,
-  y: 0,
-  width: 50,
-  height: 50,
-  blocks: [new Map(), new Map()],
-  clickAbles: [],
-  drawMode: "draw",
-  currentId: 0,
-  keyboard: new Map(),
-  clearBlocks: function () { this.blocks.forEach(map => map.clear()) }
-}
 
-ui.width = window.innerWidth - 50
-ui.height = 150
-
-ui.onclick = event => {
-  let x = event.clientX - 50
-  let y = event.clientY - window.innerHeight + 150
-  for (const area of constants.clickAbles) {
-    if (area[0] <= x && (area[0] + 24) >= x && (area[1]) <= y && (area[1] + 24) >= y) {
-      constants.currentId = area[2]
-      drawUiFull(area[3])
-      break;
-    }
-  }
-}
-
+//static variables
 const blockLayers = {
   "bg": [[60, 69], [82, 91], [109, 110]],
   "fg": [[1, 10], [12, 12], [18, 43], [45, 54], [72, 81], [95, 96], [106, 108], [111, 112]],
   "ac": [[0, 0], [11, 11], [13, 17], [44, 44], [55, 59], [70, 71], [92, 94], [97, 105]]
 }
 
-const image = new Image()
-image.src = './AllBlocks.png'
-const ground = new Image()
-ground.src = './ground.png'
-const areas = document.querySelectorAll('.ui.bottom>div:nth-child(1)>button')
+const background = new Map();
+const foreground = new Map();
+let clickAreas = [];
+const keyboard = new Map();
+let token;
+let connectionServer;
+let place = false;
 
-let areaValue = 0
+//changing variables
+let worldWidth = 50;
+let worldHeight = 50;
+let miniMapSize = 2;
+
+let currentDrawMode = "draw";
+let currentId = 0;
+let areaValue = 0;
+
+// resize canvas
+gameCanvas.width = worldWidth * 24
+gameCanvas.height = worldHeight * 24
+miniMapCanvas.width = worldWidth * miniMapSize
+miniMapCanvas.height = worldHeight * miniMapSize
+ui.width = window.innerWidth - 50
+ui.height = 150
+
+// onclickEvents
+ui.onclick = event => {
+  let x = event.clientX - 50
+  let y = event.clientY - window.innerHeight + 150
+  for (const area of clickAreas) {
+    if (area[0] <= x && (area[0] + 24) >= x && (area[1]) <= y && (area[1] + 24) >= y) {
+      currentId = area[2]
+      drawUiFull(area[3])
+      break;
+    }
+  }
+}
+
+gameCanvas.onclick = event => {
+  placeBlock(Math.floor((event.clientX - parseFloat(gameCanvas.style.left)) / 24), Math.floor((event.clientY - parseFloat(gameCanvas.style.top)) / 24), event.shiftKey)
+}
+
+gameCanvas.onmousemove = event => {
+  if (event.which) {
+    placeBlock(Math.floor((event.clientX - parseFloat(gameCanvas.style.left)) / 24), Math.floor((event.clientY - parseFloat(gameCanvas.style.top)) / 24), event.shiftKey)
+  }
+}
+
 areas.forEach(element => {
   element.value = areaValue++
   element.onclick = () => {
@@ -65,30 +90,7 @@ areas.forEach(element => {
   }
 })
 
-function drawUiFull(p) {
-  let j = 0
-  let k = 0
-  clearUi()
-  ui.width = window.innerWidth - 50
-  constants.clickAbles = []
-  while (findBlock(k) !== null) {
-    if (findBlock(k)[1] == p) {
-      if (constants.currentId == k) {
-        uiCtx.beginPath()
-        uiCtx.rect((24 + j) % (ui.width - 48) - 1, Math.floor((24 + j) / (ui.width - 48)) * 30 + 11, 26, 26)
-        uiCtx.stroke()
-      }
-      constants.clickAbles.push([(24 + j) % (ui.width - 48), Math.floor((24 + j) / (ui.width - 48)) * 30 + 12, k, p])
-      drawUi(k, (24 + j) % (ui.width - 48), Math.floor((24 + j) / (ui.width - 48)) * 30 + 12)
-      j += 25
-    }
-    k++
-  }
-}
-
-window.constants = constants
-window.gameCanvas = gameCanvas
-
+//general Functions
 function findBlock(id) {
   if (blockLayers.bg.some(list => list[1] >= id && list[0] <= id)) return [0, 2];
   if (blockLayers.fg.some(list => list[1] >= id && list[0] <= id)) return [1, 0];
@@ -96,8 +98,28 @@ function findBlock(id) {
   return null;
 }
 
-function clearUi() {
-  uiCtx.clearRect(0, 0, ui.width, ui.height)
+function getLayer(layer) {
+  switch (layer) {
+    case 0:
+      return background
+    case 1:
+      return foreground
+    default:
+      break;
+  }
+}
+
+//drawFunctions
+function drawBackgroundTiles() {
+  for (let width = 0; width < worldWidth; width++) {
+    for (let height = 0; height < worldHeight; height++) {
+      drawBackground(width, height)
+    }
+  }
+}
+
+function drawBackground(x, y) {
+  gameCtx.drawImage(ground, x * 24, y * 24, 24, 24)
 }
 
 function drawUi(id, x, y) {
@@ -105,143 +127,140 @@ function drawUi(id, x, y) {
   uiCtx.drawImage(image, (id & 15) * 25, Math.floor(id / 16) * 25, 24, 24, x, y, 24, 24)
 }
 
-let lastTime = 0;
-let token;
-let connectionServer;
-let place = false;
-
-function main(currentTime) {
-  requestAnimationFrame(main)
-  if (lastTime > currentTime) lastTime = currentTime
-  lastTime = currentTime
-  draw()
-  placeBlocks()
-  moveScreen()
-}
-
-main()
-
-function moveScreen() {
-  const speed = 0.4
-  const left = ck("has", 37) || ck("has", 65)
-  const right = ck("has", 39) || ck("has", 68)
-  const down = ck("has", 38) || ck("has", 87)
-  const up = ck("has", 40) || ck("has", 83)
-  if (down ^ up) {
-    if (down) {
-      constants.y -= speed
-    } else {
-      constants.y += speed
-    }
-  }
-  if (left ^ right) {
-    if (left) {
-      constants.x -= speed
-    } else {
-      constants.x += speed
-    }
-  }
-  constants.x = Math.max(-10, Math.min(constants.width - 15, constants.x))
-  constants.y = Math.max(-10, Math.min(constants.height - 5, constants.y))
-}
-
-function draw() {
-  if (gameCanvas.height !== window.innerHeight) gameCanvas.height = window.innerHeight
-  if (gameCanvas.width !== window.innerWidth) gameCanvas.width = window.innerWidth
-  if (miniMapCanvas.height !== constants.height * 2) miniMapCanvas.height = constants.height * 2
-  if (miniMapCanvas.width !== constants.width * 2) miniMapCanvas.width = constants.width * 2
-
-  clear()
-  drawBackground()
-  drawBlocks()
-}
-
-function clear() {
-  gameCtx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-  miniMapCtx.clearRect(0, 0, miniMapCanvas.width, miniMapCanvas.height);
-}
-
-function drawBackground() {
-  for (let i = Math.min(Math.floor(-constants.x), 0); i < constants.width; i++) {
-    let dx = (i - constants.x)
-    if (dx > Math.floor(gameCanvas.width / 24) + 1) break
-    for (let j = Math.min(Math.floor(-constants.y), 0); j < constants.height; j++) {
-      let dy = (j - constants.y)
-      if (dy > Math.floor(gameCanvas.height / 24) + 1) break
-      gameCtx.drawImage(ground, dx * 24, dy * 24, 24, 24)
-    }
-  }
-}
-
-function drawBlocks() {
-  for (const [key, value] of constants.blocks[0]) {
-    drawBlock(value.id, ...key.split(","))
-    drawMiniMap(value.id, ...key.split(","))
-  }
-
-  for (const [key, value] of constants.blocks[1]) {
-    drawBlock(value.id, ...key.split(","))
-    drawMiniMap(value.id, ...key.split(","))
-  }
-}
-
 function drawBlock(id, x, y) {
-  let dx = x - constants.x, dy = y - constants.y
-  if (!(dx < -1 || dy < -1 || gameCanvas.width / 24 < dx || gameCanvas.height / 24 < dy))
-    gameCtx.drawImage(image, (id & 15) * 25, Math.floor(id / 16) * 25, 24, 24, dx * 24, dy * 24, 24, 24)
+  gameCtx.drawImage(image, (id & 15) * 25, Math.floor(id / 16) * 25, 24, 24, x * 24, y * 24, 24, 24)
 }
 
 function drawMiniMap(id, x, y) {
   if (mapColor.get(id) == -1) return
   miniMapCtx.beginPath()
-  miniMapCtx.rect(x * 2, y * 2, 2, 2)
+  miniMapCtx.rect(x * miniMapSize, y * miniMapSize, miniMapSize, miniMapSize)
   if (mapColor.get(id) == -2) miniMapCtx.fillStyle = "#000000"
   else miniMapCtx.fillStyle = "#" + mapColor.get(id).toString(16)
   miniMapCtx.fill()
 }
 
-function placeBlocks() {
-  if (!ck("has", "mouse")) return
-  const x = Math.floor(ck("get", "mouse").X / 24 + constants.x)
-  const y = Math.floor(ck("get", "mouse").Y / 24 + constants.y)
-  if (x >= constants.width || y >= constants.height) return
-  if (x < 0 || y < 0) return
+function drawUiFull(p) {
+  let j = 0
+  let k = 0
+  clearUi()
+  clickAreas = []
+  while (findBlock(k) !== null) {
+    if (findBlock(k)[1] == p) {
+      if (currentId == k) {
+        uiCtx.beginPath()
+        uiCtx.rect((24 + j) % (ui.width - 48) - 1, Math.floor((24 + j) / (ui.width - 48)) * 30 + 11, 26, 26)
+        uiCtx.stroke()
+      }
+      clickAreas.push([(24 + j) % (ui.width - 48), Math.floor((24 + j) / (ui.width - 48)) * 30 + 12, k, p])
+      drawUi(k, (24 + j) % (ui.width - 48), Math.floor((24 + j) / (ui.width - 48)) * 30 + 12)
+      j += 25
+    }
+    k++
+  }
+}
+
+//clear functions
+function clear() {
+  gameCtx.clearRect(0, 0, gameCanvas.width, gameCanvas.height)
+}
+
+function clearMinimap() {
+  miniMapCtx.clearRect(0, 0, miniMapCanvas.width, miniMapCanvas.height)
+}
+
+function clearUi() {
+  uiCtx.clearRect(0, 0, ui.width, ui.height)
+}
+
+//movement
+function moveScreen() {
+  const speed = 10
+  const left = keyboard.has(37) || keyboard.has(65)
+  const right = keyboard.has(39) || keyboard.has(68)
+  const down = keyboard.has(38) || keyboard.has(87)
+  const up = keyboard.has(40) || keyboard.has(83)
+  if (down ^ up) {
+    if (down) {
+      gameCanvas.style.top = parseFloat(gameCanvas.style.top) + speed + "px"
+    } else {
+      gameCanvas.style.top = parseFloat(gameCanvas.style.top) - speed + "px"
+    }
+  }
+  if (left ^ right) {
+    if (left) {
+      gameCanvas.style.left = parseFloat(gameCanvas.style.left) + speed + "px"
+    } else {
+      gameCanvas.style.left = parseFloat(gameCanvas.style.left) - speed + "px"
+    }
+  }
+}
+
+function drawCanvas() {
+  clear()
+  clearMinimap()
+  drawBackgroundTiles()
+  for (const [key, value] of background) {
+    drawBlock(value.id, ...key.split(","))
+    drawMiniMap(value.id, ...key.split(","))
+  }
+  for (const [key, value] of foreground) {
+    drawBlock(value.id, ...key.split(","))
+    drawMiniMap(value.id, ...key.split(","))
+  }
+}
+
+gameCanvas.style.left = "0px"
+gameCanvas.style.top = "0px"
+function loop() {
+  requestAnimationFrame(loop)
+  moveScreen()
+}
+window.gameCanvas = gameCanvas
+loop()
+window.onload = () => {
+  drawCanvas()
+}
+
+function placeBlock(x, y, empty) {
   const key = `${x},${y}`
-  const layer = findBlock(constants.currentId)[0]
+  switch (currentDrawMode) {
+    case "draw":
+      if (currentId === 0 || empty) {
+        if (foreground.has(key)) {
+          foreground.delete(key)
+        } else if (background.has(key)) {
+          background.delete(key)
+        }
+      } else {
+        getLayer(findBlock(currentId)[0]).set(key, new Block(currentId))
+      }
 
-  if (ck("has", 16)) {
-    switch (constants.drawMode) {
-      case "draw":
-        if (constants.blocks[1].has(key)) constants.blocks[1].delete(key)
-        if (constants.blocks[0].has(key)) constants.blocks[0].delete(key)
-        break;
+      drawMiniMap(12, x, y)
+      drawBackground(x, y)
+      if (background.get(key)) {
+        drawBlock(background.get(key).id, x, y)
+        drawMiniMap(background.get(key).id, x, y)
+      }
+      if (foreground.get(key)) {
+        drawBlock(foreground.get(key).id, x, y)
+        drawMiniMap(foreground.get(key).id, x, y)
+      }
+      break;
+    case "fill":
+      console.log(true)
+      let bg = background.get(key)
+      let fg = foreground.get(key)
+      if (!bg) bg = new Block(0)
+      if (!fg) fg = new Block(0)
 
-      case "fill":
-        let bg = constants.blocks[0].get(key)
-        let fg = constants.blocks[1].get(key)
-
-        if (bg === undefined) bg = new Block(0)
-        if (fg === undefined) fg = new Block(0)
-
+      if (empty) {
         fill(x, y, bg, fg, null)
-        break;
-    }
-  } else {
-    switch (constants.drawMode) {
-      case "draw":
-        if (constants.currentId !== 0)
-          constants.blocks[layer].set(key, new Block(constants.currentId))
-        break;
-      case "fill":
-        let bg = constants.blocks[0].get(key)
-        let fg = constants.blocks[1].get(key)
-
-        if (bg === undefined) bg = new Block(0)
-        if (fg === undefined) fg = new Block(0)
-
-        fill(x, y, bg, fg, new Block(constants.currentId))
-        break;
-    }
+      } else {
+        fill(x, y, bg, fg, new Block(currentId))
+      }
+      drawCanvas()
+      break;
   }
 }
 
@@ -251,19 +270,19 @@ function fill(x, y, bg, fg, newBlock) {
   if (newBlock instanceof Block) {
     const layer = findBlock(newBlock.id)[0]
     if (newBlock.id === 0)
-      constants.blocks[layer].delete(`${x},${y}`)
+      getLayer(layer).delete(`${x},${y}`)
     else
-      constants.blocks[layer].set(`${x},${y}`, newBlock)
+      getLayer(layer).set(`${x},${y}`, newBlock)
     for (const direction of directions) {
-      if (x + direction[0] < constants.width && x + direction[0] >= 0 && y + direction[1] < constants.height && y + direction[1] >= 0) {
-        let oldBg = constants.blocks[0].get(`${x + direction[0]},${y + direction[1]}`)
-        let oldFg = constants.blocks[1].get(`${x + direction[0]},${y + direction[1]}`)
+      if (x + direction[0] < worldWidth && x + direction[0] >= 0 && y + direction[1] < worldHeight && y + direction[1] >= 0) {
+        let oldBg = background.get(`${x + direction[0]},${y + direction[1]}`)
+        let oldFg = foreground.get(`${x + direction[0]},${y + direction[1]}`)
 
         if (oldBg === undefined) oldBg = new Block(0)
         if (oldFg === undefined) oldFg = new Block(0)
 
         if (oldBg.equals(bg) && oldFg.equals(fg)) {
-          let oldBlock = constants.blocks[layer].get(`${x + direction[0]},${y + direction[1]}`)
+          let oldBlock = getLayer(layer).get(`${x + direction[0]},${y + direction[1]}`)
           if (oldBlock === undefined) oldBlock = new Block(0)
           if (!oldBlock.equals(newBlock))
             fill(x + direction[0], y + direction[1], bg, fg, newBlock)
@@ -271,12 +290,12 @@ function fill(x, y, bg, fg, newBlock) {
       }
     }
   } else {
-    constants.blocks[0].delete(`${x},${y}`)
-    constants.blocks[1].delete(`${x},${y}`)
+    background.delete(`${x},${y}`)
+    foreground.delete(`${x},${y}`)
     for (const direction of directions) {
-      if (x + direction[0] < constants.width && x + direction[0] >= 0 && y + direction[1] < constants.height && y + direction[1] >= 0) {
-        let oldBg = constants.blocks[0].get(`${x + direction[0]},${y + direction[1]}`)
-        let oldFg = constants.blocks[1].get(`${x + direction[0]},${y + direction[1]}`)
+      if (x + direction[0] < worldWidth && x + direction[0] >= 0 && y + direction[1] < worldHeight && y + direction[1] >= 0) {
+        let oldBg = background.get(`${x + direction[0]},${y + direction[1]}`)
+        let oldFg = foreground.get(`${x + direction[0]},${y + direction[1]}`)
 
         if (oldBg === undefined) oldBg = new Block(0)
         if (oldFg === undefined) oldFg = new Block(0)
@@ -290,37 +309,16 @@ function fill(x, y, bg, fg, newBlock) {
 
 }
 
-function ck(mode, key, value = "") {
-  return constants.keyboard[mode](key, value)
-}
-
-function mouseMove(e) {
-  e.preventDefault();
-  if (!e.composedPath().includes(gameCanvas)) return
-  ck("set", "mouse", { X: e.clientX, Y: e.clientY });
-}
-
-window.addEventListener("mousedown", e => {
-  if (e.composedPath().indexOf(top) == -1) mouseMove(e)
-})
-
-window.addEventListener("mousemove", e => {
-  if (!ck("has", "mouse")) return
-  mouseMove(e)
-})
-
 window.addEventListener("keydown", e => {
   if (e.composedPath().indexOf(top) == -1) {
-    ck("set", e.keyCode, true)
+    keyboard.set(e.keyCode, true)
   }
 })
 
 window.addEventListener("keyup", e => {
-  if (e.composedPath().indexOf(top) == -1) ck("delete", e.keyCode)
-})
-
-window.addEventListener("mouseup", e => {
-  ck("delete", "mouse")
+  if (e.composedPath().indexOf(top) == -1) {
+    keyboard.delete(e.keyCode)
+  }
 })
 
 window.addEventListener("message", event => {
@@ -335,9 +333,9 @@ window.addEventListener("message", event => {
     } else {
       const value = JSON.parse(event.data);
       if (value[1]) {
-        ck("set", value[0], true);
+        keyboard.set(value[0], true);
       } else {
-        ck("delete", value[0]);
+        keyboard.delete(value[0]);
       }
     }
   }
@@ -395,32 +393,36 @@ async function connect(authToken) {
 }
 
 function BlockHandeler(initMessage) { //false = 0
-  constants.clearBlocks()
-  constants.width = initMessage.get(9)
-  constants.height = initMessage.get(10)
+  foreground.clear()
+  background.clear()
+  worldWidth = initMessage.get(9)
+  worldHeight = initMessage.get(10)
+  gameCanvas.style.left = "0px"
+  gameCanvas.style.top = "0px"
   let index = 0;
-  for (let y = 0; y < constants.height; y++) {
-    for (let x = 0; x < constants.width; x++) {
+  for (let y = 0; y < worldHeight; y++) {
+    for (let x = 0; x < worldWidth; x++) {
       let blocks = initMessage.get(11 + index);
       if (blocks === false) blocks = 0;
-      let foreground = EEUniverse.getFgId(blocks);
-      let background = EEUniverse.getBgId(blocks);
+      let foregroundBlock = EEUniverse.getFgId(blocks);
+      let backgroundBlock = EEUniverse.getBgId(blocks);
       let argumentList = [];// not yet used
       index++
-      let maxArguments = args.get(foreground).length
+      let maxArguments = args.get(foregroundBlock).length
       for (let i = 0; i < maxArguments; i++) {
         argumentList.push(initMessage.get(11 + index++));
       }
-      const bg = new Block(background) //this way both fgs and bgs can have arguments. for later implementation though
-      const fg = new Block(foreground, ...argumentList);
+      const bg = new Block(backgroundBlock) //this way both fgs and bgs can have arguments. for later implementation though
+      const fg = new Block(foregroundBlock, ...argumentList);
       if (bg.id !== 0) {
-        constants.blocks[0].set(`${x},${y}`, bg)
+        background.set(`${x},${y}`, bg)
       }
       if (fg.id !== 0) {
-        constants.blocks[1].set(`${x},${y}`, fg)
+        foreground.set(`${x},${y}`, fg)
       }
     }
   }
+  drawCanvas()
 }
 
 function CheckDifference(initMessage) {
@@ -431,19 +433,19 @@ function CheckDifference(initMessage) {
     for (let x = 0; x < width; x++) {
       let blocks = initMessage.get(11 + index);
       if (blocks === false) blocks = 0;
-      let foreground = EEUniverse.getFgId(blocks);
-      let background = EEUniverse.getBgId(blocks);
+      let foregroundBlock = EEUniverse.getFgId(blocks);
+      let backgroundBlock = EEUniverse.getBgId(blocks);
       let argumentList = [];
       index++
-      let maxArguments = args.get(foreground).length
+      let maxArguments = args.get(foregroundBlock).length
       for (let i = 0; i < maxArguments; i++) {
         argumentList.push(initMessage.get(11 + index++));
       }
-      const bg = new Block(background);
-      const fg = new Block(foreground, ...argumentList);
+      const bg = new Block(backgroundBlock);
+      const fg = new Block(foregroundBlock, ...argumentList);
 
-      let newBg = constants.blocks[0].get(`${x},${y}`)
-      let newFg = constants.blocks[1].get(`${x},${y}`)
+      let newBg = background.get(`${x},${y}`)
+      let newFg = foreground.get(`${x},${y}`)
 
       if (newBg === undefined) newBg = new Block(0)
       if (newFg === undefined) newFg = new Block(0)
